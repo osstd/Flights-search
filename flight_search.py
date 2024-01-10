15cfb677cf721261e48c09e0538bba900e6f27fd
Omar Shemy <OmarShemy@Omar-Shemys-MacBook-Pro.local>
2024-01-10 17:02:47 -0500

    currency error handling

--- flight_search.py
+++ flight_search.py
@@ -11,19 +11,31 @@ headers = {
 
 
 class FlightSearch:
+
     def check_code(self, city_name):
 
         query = {
             'term': city_name,
             'location_types': "city",
         }
-        response = requests.get(url=f"{TEQUILA_ENDPOINT}/locations/query", params=query, headers=headers)
-        response.raise_for_status()
-        result = response.json()["locations"]
-        code = result[0]['code']
-        return code
+        try:
+            response = requests.get(url=f"{TEQUILA_ENDPOINT}/locations/query", params=query, headers=headers)
+            response.raise_for_status()
+            try:
+                result = response.json()["locations"]
+                code = result[0]['code']
+                return code, None
+            except IndexError:
+                code = None
+                return code, None
+        except requests.exceptions.HTTPError as err:
+            return None, err
+        except requests.exceptions.RequestException as err:
+            return None, err
+        except Exception as err:
+            return None, err
 
-    def find_flights(self, origin_city_code, destination_city_code, from_time, to_time, nights_from, nights_to, stops):
+    def find_flights(self, origin_city_code, destination_city_code, from_time, to_time, nights_from, nights_to, stops, currency):
         query = {
             'fly_from': origin_city_code,
             'fly_to': destination_city_code,
@@ -33,23 +45,30 @@ class FlightSearch:
             'nights_in_dst_to': nights_to,
             'flight_type': 'round',
             'max_stopovers': stops,
-            'curr': 'USD',
+            'curr': currency,
         }
-        response = requests.get(url=f"{TEQUILA_ENDPOINT}/search", params=query, headers=headers)
-        response.raise_for_status()
         try:
-            data = response.json()["data"][0]
-        except IndexError:
-            return None
-
-        flight_data = FlightData(
-            price=data["price"],
-            origin_city=data["route"][0]["cityFrom"],
-            origin_airport=data["route"][0]["flyFrom"],
-            destination_city=data["route"][0]["cityTo"],
-            destination_airport=data["route"][0]["flyTo"],
-            out_date=datetime.utcfromtimestamp(data["route"][0]['dTimeUTC']).strftime('%Y-%m-%d %H:%M:%S UTC'),
-            return_date=datetime.utcfromtimestamp(data["route"][1]['dTimeUTC']).strftime('%Y-%m-%d %H:%M:%S UTC'),
-        )
-
-        return flight_data
+            response = requests.get(url=f"{TEQUILA_ENDPOINT}/search", params=query, headers=headers)
+            response.raise_for_status()
+            try:
+                data = response.json()["data"][0]
+                flight_data = FlightData(
+                    price=data["price"],
+                    origin_city=data["route"][0]["cityFrom"],
+                    origin_airport=data["route"][0]["flyFrom"],
+                    destination_city=data["route"][0]["cityTo"],
+                    destination_airport=data["route"][0]["flyTo"],
+                    out_date=datetime.utcfromtimestamp(data["route"][0]['dTimeUTC']).strftime('%Y-%m-%d %H:%M:%S UTC'),
+                    return_date=datetime.utcfromtimestamp(data["route"][1]['dTimeUTC']).strftime(
+                        '%Y-%m-%d %H:%M:%S UTC'),
+                )
+                return flight_data, None
+            except IndexError:
+                flight_data = None
+                return flight_data, None
+        except requests.exceptions.HTTPError as err:
+            return None, err
+        except requests.exceptions.RequestException as err:
+            return None, err
+        except Exception as err:
+            return None, err
--- main.py
+++ main.py
@@ -62,27 +62,44 @@ def flight_details():
             max_stopovers = int(request.form['stops'])
         else:
             max_stopovers = 0
-
-        results_ = flight_search(from_time, to_time, nights_in_dst_from, nights_in_dst_to, max_stopovers)
+        currency = request.form['currency']
+        results_ = flight_search(from_time, to_time, nights_in_dst_from, nights_in_dst_to, max_stopovers, currency)
 
         return render_template('index.html', cities=results_)
     return render_template('index.html', details=True)
 
 
-def flight_search(from_time, to_time, nights_from, nights_to, stops):
+def flight_search(from_time, to_time, nights_from, nights_to, stops, currency):
     origin_city['iataCode'] = search.check_code(origin_city['city'])
     results = []
     for city in cities:
         if city['city']:
-            city['iataCode'] = search.check_code(city['city'])
-            flight = search.find_flights(origin_city['iataCode'], city['iataCode'], from_time, to_time, nights_from,
-                                         nights_to,
-                                         stops)
+            city['iataCode'], err = search.check_code(city['city'])
+            if not city['iataCode']:
+                city['result'] = f"{city['city'].capitalize()}:\nNo iataCode was found for this city, please check name and try again"
+                results.append(city['result'])
+                continue
+            elif err:
+                city['result'] = f"{city['city'].capitalize()}:\nError, {err}"
+                results.append(city['result'])
+                continue
+            else:
+                flight, err = search.find_flights(origin_city['iataCode'], city['iataCode'], from_time, to_time,
+                                                  nights_from,
+                                                  nights_to, stops, currency)
 
-            city['result'] = f"Found a route! Only ${flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
-            results.append(city['result'])
+                if not flight:
+                    city['result'] = f"{city['city'].capitalize()}:\nNo flight results were found for this search " \
+                                     f"criteria"
+                elif err:
+                    city['result'] = f"{city['city'].capitalize()}:\nError, {err}"
+                else:
+                    city['result'] = f"{city['city'].capitalize()},\nfound a route!\n Only {currency}{flight.price} to fly from {flight.origin_city}-" \
+                                     f"{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, " \
+                                     f"from {flight.out_date} to {flight.return_date}."
+                results.append(city['result'])
     return results
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
